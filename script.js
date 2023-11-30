a4e68a9ee24c9cee01ed6e8e191d7907076fa93e
Jack Michell <123218979+JackMichell@users.noreply.github.com>
2023-11-30 14:55:03 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const playerScoreDisplay = document.getElementById("playerScore");
+const computerScoreDisplay = document.getElementById("computerScore");
+
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPlay() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playerChoice() {
+  const buttons = document.querySelectorAll("button");
+
+  return buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      return button.id;
+    });
+  });
+}
+
+function playRound() {
+  const computerSelection = computerPlay();
+
+  switch (computerSelection) {
+    case "rock":
+      if (playerChoice() === "rock") {
+        alert("It's a tie!");
+      } else if (playerChoice() === "paper") {
+        alert("You win! Paper beats rock.");
+        playerScore++;
+      } else {
+        alert("You lose! Rock beats scissors.");
+        computerScore++;
+      }
+      break;
+    case "paper":
+      if (playerChoice() === "rock") {
+        alert("You lose! Paper beats rock.");
+        computerScore++;
+      } else if (playerChoice() === "paper") {
+        alert("It's a tie!");
+      } else {
+        alert("You win! Scissors beats paper.");
+        playerScore++;
+      }
+      break;
+    case "scissors":
+      if (playerChoice() === "rock") {
+        alert("You lose! Rock beats scissors.");
+        playerScore++;
+      } else if (playerChoice() === "paper") {
+        alert("You win! Scissors beats paper.");
+        computerScore++;
+      } else {
+        alert("It's a tie!");
+      }
+      break;
+  }
+
+  // Update the HTML elements with the new scores
+  playerScoreDisplay.textContent = playerScore.toString();
+  computerScoreDisplay.textContent = computerScore.toString();
+}
